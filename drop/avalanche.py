431b46c45d05da08b0436687f44c6c058939d617
Yash <sreyanshu@mail.ru>
2025-08-20 12:24:54 +0530

    Update and rename avalanche.js to avalanche.py

---
+++ drop/avalanche.py
@@ -0,0 +1,14 @@
+AVALANCHE_RPC_PROIVDERS = '''
+https://avalanche-c-chain-rpc.publicnode.com
+https://1rpc.io/avax/c
+https://endpoints.omniatech.io/v1/avax/mainnet/public
+https://api.avax.network/ext/bc/C/rpc
+https://avalanche.drpc.org
+https://avalanche-mainnet.wallet.brave.com/
+https://rpc.ankr.com/avalanche
+https://endpoints.omniatech.io/v1/avax/mainnet/public
+https://ava-mainnet.public.blastapi.io/ext/bc/C/avax
+https://avalanche-c-chain.publicnode.com
+
+
+https://avalanche-mainnet.core.chainstack.com/ext/bc/C/rpc/606abf17c5f4df22bbe4be01edeb9c2b
